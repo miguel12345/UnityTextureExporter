eddf314c935855dd0ead222535043ca90d6e277d
Miguel Ferreira <amigueloferreira@gmail.com>
2017-08-27 22:30:56 +0100

    Added support for packed sprites. Fixes #1

--- Assets/TextureExporter/Editor/TextureExporter.cs
+++ Assets/TextureExporter/Editor/TextureExporter.cs
@@ -11,45 +11,49 @@ public class TextureExporter {
 	private static void ExportSpriteRendererTextureToPng(MenuCommand command)
 	{
 		var spriteRenderer = (SpriteRenderer)command.context;
-		ExportTexture (spriteRenderer.sprite.texture);
+		var sprite = spriteRenderer.sprite;
+		ExportTexture (sprite.texture,sprite.textureRect,sprite.name);
 	}
 
 	[MenuItem("CONTEXT/RawImage/Export texture to png")]
 	private static void ExportRawImageTextureToPng(MenuCommand command)
 	{
 		var Image = (RawImage)command.context;
-		ExportTexture (Image.texture);
+		var texture = Image.texture;
+		ExportTexture (texture,new Rect(0,0,texture.width,texture.height),texture.name);
 	}
 
 	[MenuItem("CONTEXT/Image/Export texture to png")]
 	private static void ExportImageTextureToPng(MenuCommand command)
 	{
 		var Image = (Image)command.context;
-		ExportTexture (Image.sprite.texture);
+		var sprite = Image.sprite;
+		ExportTexture (sprite.texture,sprite.textureRect,sprite.name);
 	}
 
 	[MenuItem("CONTEXT/MeshRenderer/Export main texture to png")]
 	private static void ExportMeshRendererMainTextureToPng(MenuCommand command)
 	{
 		var meshRenderer = (MeshRenderer)command.context;
-		ExportTexture (meshRenderer.sharedMaterial.mainTexture);
+		var texture = meshRenderer.sharedMaterial.mainTexture;
+		ExportTexture (texture,new Rect(0,0,texture.width,texture.height),texture.name);
 	}
 
-	private static void ExportTexture(Texture sourceTexture) {
+	private static void ExportTexture(Texture sourceTexture,Rect textureRect, string name) {
 		
 		RenderTexture rt = RenderTexture.GetTemporary(sourceTexture.width,sourceTexture.height,0,RenderTextureFormat.ARGB32,RenderTextureReadWrite.Default);
 		Graphics.Blit(sourceTexture,rt);
 
 		RenderTexture.active = rt;
 
-		Texture2D readableTexture = new Texture2D(sourceTexture.width,sourceTexture.height,TextureFormat.ARGB32,false);
-		readableTexture.ReadPixels(new Rect(0,0,sourceTexture.width,sourceTexture.height),0,0 );
+		Texture2D readableTexture = new Texture2D((int)textureRect.width,(int)textureRect.height,TextureFormat.ARGB32,false);
+		readableTexture.ReadPixels(textureRect,0,0 );
 
 		RenderTexture.active = null;
 		RenderTexture.ReleaseTemporary(rt);
 		var pngBytes = readableTexture.EncodeToPNG();
 
-		var savePath = EditorUtility.SaveFilePanel("Save image", "", sourceTexture.name, "png");
+		var savePath = EditorUtility.SaveFilePanel("Save image", "", name, "png");
 
 		if (string.IsNullOrEmpty(savePath))
 		{
